ea9288e8c1f9c94c43e7d55b4d239bf7373bfd3a
chris-jan-trapp <christian.trapp@gbv.de>
2022-11-08 13:14:25 +0100

    Dumped servicer client class.

--- src/server/servicer_client.py
+++ src/server/servicer_client.py
@@ -17,65 +17,65 @@ DATABASE_CALLBACKS = ['db_pre_update_one',
                       'db_post_delete_one',
                       'db_post_delete']
 
-class ServicerClient:
-    def configure(self, url, routing, logger):
-        self.logger = logger
-        self.url = url
-        self.routing = routing
-        self.logger.info('Added URL and routing,')
+def redirect(self, hook, easydb_context, easydb_info):
+    logger = easydb_context.get_logger('pf.server.plugin.servicer')
+    settings = easydb_context.get_config('base.system.servicer_client')
+    session = easydb_context.get_session()
+    data = easydb_info.get('data')
+
+    servicer_url = settings.get('servicer_url', "")
     
-    def redirect(self, hook, easydb_context, easydb_info):
-        self.logger.debug('Latching into ' + hook)
-        session = easydb_context.get_session()
-        data = easydb_info.get('data')
-        self.logger.debug(str(data))
-        object_type = next(data.keys())
-        served_types = self.routing[hook]
-        self.logger.debug(f'Looking for redirect for {object_type} in {hook}.')
-        if object_type in served_types or '*' in served_types:
-            full_url = join(self.url, hook, object_type)
-            try:
-                self.logger.info("\n".join(["Redirecting:", full_url, str(session), str(data)]))
+    routing_json = settings.get('routing',  '{}')
+    routing = json.loads(routing_json)
 
-                response = requests.post(full_url,
-                                        json={'session': session, "data": data},
-                                        headers={'Content-type': 'application/json'})
-                
-                if response.ok:
-                    data = response.json()['data']
-                    self.logger.debug("Servicer returned data: " + json.dumps(data, indent=2))
-                else:
-                    self.logger.error("Servicer failed with: " + str(response.content))  
-            except Exception as exception:
-                self.logger.error(str(exception))
+    logger.debug('Latching into ' + hook)
+    
+    logger.debug(str(data))
+    object_type = next(data.keys())
+    served_types = routing[hook]
+    logger.debug(f'Looking for redirect for {object_type} in {hook}.')
+    if object_type in served_types or '*' in served_types:
+        full_url = join(servicer_url, hook, object_type)
+        try:
+            logger.info("\n".join(["Redirecting:", full_url, str(session), str(data)]))
+
+            response = requests.post(full_url,
+                                    json={'session': session, "data": data},
+                                    headers={'Content-type': 'application/json'})
             
-        return data
-
-client = ServicerClient()
+            if response.ok:
+                data = response.json()['data']
+                logger.debug("Servicer returned data: " + json.dumps(data, indent=2))
+            else:
+                logger.error("Servicer failed with: " + str(response.content))  
+        except Exception as exception:
+            logger.error(str(exception))
+        
+    return data
 
 def latch_db_pre_update_one(easydb_context, easydb_info):
-    return client.redirect('db_pre_update_one', easydb_context, easydb_info)
+    return redirect('db_pre_update_one', easydb_context, easydb_info)
         
 def latch_db_pre_update(easydb_context, easydb_info):
-    return client.redirect('db_pre_update', easydb_context, easydb_info)
+    return redirect('db_pre_update', easydb_context, easydb_info)
         
 def latch_db_pre_delete_one(easydb_context, easydb_info):
-    return client.redirect('db_pre_delete_one', easydb_context, easydb_info)
+    return redirect('db_pre_delete_one', easydb_context, easydb_info)
         
 def latch_db_pre_delete(easydb_context, easydb_info):
-    return client.redirect('db_pre_delete', easydb_context, easydb_info)
+    return redirect('db_pre_delete', easydb_context, easydb_info)
         
 def latch_db_post_update_one(easydb_context, easydb_info):
-    return client.redirect('db_post_update_one', easydb_context, easydb_info)
+    return redirect('db_post_update_one', easydb_context, easydb_info)
         
 def latch_db_post_update(easydb_context, easydb_info):
-    return client.redirect('db_post_update', easydb_context, easydb_info)
+    return redirect('db_post_update', easydb_context, easydb_info)
         
 def latch_db_post_delete_one(easydb_context, easydb_info):
-    return client.redirect('db_post_delete_one', easydb_context, easydb_info)
+    return redirect('db_post_delete_one', easydb_context, easydb_info)
         
 def latch_db_post_delete(easydb_context, easydb_info):
-    return client.redirect('db_post_delete', easydb_context, easydb_info)
+    return redirect('db_post_delete', easydb_context, easydb_info)
         
 def easydb_server_start(easydb_context):
     settings = easydb_context.get_config('base.system.servicer_client')
@@ -84,12 +84,8 @@ def easydb_server_start(easydb_context):
     if not servicer_url:
         logger.warning('No servicer url provided in base config')
 
-    routing = settings.get('routing', False)
-    if not routing:
-        routing = '{}'
-    routing = json.loads(routing)
-
-    client.configure(servicer_url, routing, logger)
+    rules = settings.get('routing', '{}')
+    routing = json.loads(rules)
 
     for hook in routing.keys():
         latch = 'latch_' + hook
